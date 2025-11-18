f2f0e32b59d61f3047dfb999ea3736a2fc68e5d9
Shamiul40 <shamiul4040@gmail.com>
2025-11-18 22:18:00 +0600

    avoid deeply nested

--- module-2/src/Components/GroupState.jsx
+++ module-2/src/Components/GroupState.jsx
@@ -38,49 +38,49 @@
 
 
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export default function FeedbackForm() {
-  const [text, setText] = useState('');
-  const [status, setStatus] = useState('typing');
+// export default function FeedbackForm() {
+//   const [text, setText] = useState('');
+//   const [status, setStatus] = useState('typing');
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus('sending');
-    await sendMessage(text);
-    setStatus('sent');
-  }
+//   async function handleSubmit(e) {
+//     e.preventDefault();
+//     setStatus('sending');
+//     await sendMessage(text);
+//     setStatus('sent');
+//   }
 
-  const isSending = status === 'sending';
-  const isSent = status === 'sent';
+//   const isSending = status === 'sending';
+//   const isSent = status === 'sent';
 
-  if (isSent) {
-    return <h1>Thanks for feedback!</h1>
-  }
+//   if (isSent) {
+//     return <h1>Thanks for feedback!</h1>
+//   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <p>How was your stay at The Prancing Pony?</p>
-      <textarea
-        disabled={isSending}
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <br />
-      <button
-        disabled={isSending}
-        type="submit"
-      >
-        Send
-      </button>
-      {isSending && <p>Sending...</p>}
-    </form>
-  );
-}
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <p>How was your stay at The Prancing Pony?</p>
+//       <textarea
+//         disabled={isSending} 
+//         value={text}
+//         onChange={e => setText(e.target.value)}
+//       />
+//       <br />
+//       <button
+//         disabled={isSending}
+//         type="submit"
+//       >
+//         Send
+//       </button>
+//       {isSending && <p>Sending...</p>}
+//     </form>
+//   );
+// }
 
-// Pretend to send a message.
-function sendMessage(text) {
-  return new Promise(resolve => {
-    setTimeout(resolve, 2000);
-  });
-}
+// // Pretend to send a message.
+// function sendMessage(text) {
+//   return new Promise(resolve => {
+//     setTimeout(resolve, 2000);
+//   });
+// }
