742ab16205ea227f7ecc34b4cf9740646e12dfb4
Shamiul40 <shamiul4040@gmail.com>
2025-11-21 22:18:01 +0600

    toggle

--- module-2/src/Components/Accordian/Accordian.jsx
+++ module-2/src/Components/Accordian/Accordian.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChildAccordian from './ChildAccordian'
 
 export default function Accordian() {
+    const [show, setShow] = useState(false)
+
+    const handleShow =()=>{
+        setShow(!show)
+    }
+
   return (
     <div>
       <h1>Shamiul Islam Accordian</h1>
-      <ChildAccordian></ChildAccordian>
-      <ChildAccordian></ChildAccordian>
+      <ChildAccordian show={show} handleShow={handleShow}></ChildAccordian>
+      <ChildAccordian show={show} handleShow={handleShow}></ChildAccordian>
     </div>
   )
 }
