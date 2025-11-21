c7d38acca9504dd01d9a63bfd4be62dff10fc071
Shamiul40 <shamiul4040@gmail.com>
2025-11-21 21:50:42 +0600

    add

--- module-2/src/Components/ArrayInState.jsx
+++ module-2/src/Components/ArrayInState.jsx
@@ -1,22 +1,30 @@
-export default function Form({
-  status = 'empty'
-}) {
-  if (status === 'success') {
-    return <h1>That's right!</h1>
-  }
+import { useState } from 'react';
+
+export default function Accordion() {
   return (
     <>
-      <h2>City quiz</h2>
-      <p>
-        In which city is there a billboard that turns air into drinkable water?
-      </p>
-      <form>
-        <textarea />
-        <br />
-        <button>
-          Submit
-        </button>
-      </form>
+      <h2>Almaty, Kazakhstan</h2>
+      <Panel title="About" isActive={true}>
+        With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+      </Panel>
+      <Panel title="Etymology" isActive={true}>
+        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+      </Panel>
     </>
-  )
+  );
+}
+
+function Panel({ title, children, isActive }) {
+  return (
+    <section className="panel">
+      <h3>{title}</h3>
+      {isActive ? (
+        <p>{children}</p>
+      ) : (
+        <button onClick={() => setIsActive(true)}>
+          Show
+        </button>
+      )}
+    </section>
+  );
 }
