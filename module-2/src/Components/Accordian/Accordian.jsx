dbec1b666713aa586b542c89fe14a68dc88aebce
Shamiul40 <shamiul4040@gmail.com>
2025-11-21 22:23:23 +0600

    ot

--- module-2/src/Components/Accordian/Accordian.jsx
+++ module-2/src/Components/Accordian/Accordian.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import ChildAccordian from './ChildAccordian'
 
 export default function Accordian() {
-    const [show, setShow] = useState(false)
+    const [show, setShow] = useState(0)
 
     const handleShow =()=>{
         setShow(!show)
