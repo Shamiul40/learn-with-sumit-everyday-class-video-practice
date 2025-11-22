9f21c31f0da4d8da663aa6e13a26801acfd5ae58
Shamiul40 <shamiul4040@gmail.com>
2025-11-22 22:59:19 +0600

    a

--- module-2/src/Components/Accordian/ChildAccordian.jsx
+++ module-2/src/Components/Accordian/ChildAccordian.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 
 export default function ChildAccordian({show, handleShow}) {
   return (
-    <div className=' border-1 w-1/4 space-y-2 m-2 p-2'>
-        <h2>About</h2>
-       {show  ?  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sed perferendis voluptatum laborum nihil eaque esse ipsum, et rem molestiae atque ut ad voluptatem fuga quisquam repudiandae id est blanditiis deleniti doloremque quo. Impedit illo ipsa alias ea sapiente fuga amet perferendis, optio autem rerum dolorum similique facere maxime inventore.</p> : <button onClick={handleShow} className='btn bg-neutral-300 p-2 rounded-xl'>show</button> }
-        
-    </div>
+
   )
 }
