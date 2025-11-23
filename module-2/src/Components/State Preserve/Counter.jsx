88b4ab30318978a6ef18ad0d45596ac5d8526ed6
Shamiul40 <shamiul4040@gmail.com>
2025-11-23 22:33:11 +0600

    sc

--- module-2/src/Components/State Preserve/Counter.jsx
+++ module-2/src/Components/State Preserve/Counter.jsx
@@ -31,7 +31,8 @@ export default function Counter() {
     const [score, setScore]= useState(0)
   return (
     <div>
-      
+      <h1>{score}</h1>
+      <button onClick={()=>setScore(score+1)}>Add one</button>
     </div>
   )
 }
