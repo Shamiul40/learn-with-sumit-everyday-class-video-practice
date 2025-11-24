f0986233e910e7f96faf6b2717e7f1b33eb80500
Shamiul40 <shamiul4040@gmail.com>
2025-11-24 21:09:03 +0600

    change

--- module-2/src/App.jsx
+++ module-2/src/App.jsx
@@ -1,33 +1,4 @@
 
-// import './App.css'
-// import "./styles.css";
-
-
-
-// import Counter from './Components/State Preserve/Counter'
-// import { useState } from 'react';
-
-
-
-// function App() {
-// const [playerA, setPlayerA] =  useState(true)
-
-//   return (
-//     <>
-    
-//     {playerA ? <Counter title="tailors score" /> :   <Counter title = "sarah score" /> }
-
-//      <button onClick={()=>setPlayerA(!playerA)}>next Player</button>
-    
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-
 import './App.css'
 import "./styles.css";
 
@@ -44,15 +15,8 @@ const [playerA, setPlayerA] =  useState(true)
   return (
     <>
     
-    {/* {playerA ? <Counter title="tailors score" /> :   <Counter title = "sarah score" /> } */}
+    {playerA ? <Counter title="tailors score" /> :   <Counter title = "sarah score" /> }
 
-    {
-      playerA &&  <Counter title="tailors score" />
- 
-    }
-
-    {
-      !playerA &&  <Counter title = "sarah score" />   }
      <button onClick={()=>setPlayerA(!playerA)}>next Player</button>
     
     </>
@@ -60,3 +24,39 @@ const [playerA, setPlayerA] =  useState(true)
 }
 
 export default App
+
+
+
+
+// import './App.css'
+// import "./styles.css";
+
+
+
+// import Counter from './Components/State Preserve/Counter'
+// import { useState } from 'react';
+
+
+
+// function App() {
+// const [playerA, setPlayerA] =  useState(true)
+
+//   return (
+//     <>
+    
+//     {/* {playerA ? <Counter title="tailors score" /> :   <Counter title = "sarah score" /> } */}
+
+//     {
+//       playerA &&  <Counter title="tailors score" />
+ 
+//     }
+
+//     {
+//       !playerA &&  <Counter title = "sarah score" />   }
+//      <button onClick={()=>setPlayerA(!playerA)}>next Player</button>
+    
+//     </>
+//   )
+// }
+
+// export default App
