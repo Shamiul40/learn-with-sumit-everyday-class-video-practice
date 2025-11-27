30476e4a809396620a725a644f3714b822361dc7
Shamiul40 <shamiul4040@gmail.com>
2025-11-27 21:20:54 +0600

    context 2

--- module-2/src/App.jsx
+++ module-2/src/App.jsx
@@ -1,16 +1,16 @@
+import Heading from "./Components/ContextApi/Heading";
+import Section from "./Components/ContextApi/Section";
 
-import Heading from './Heading.js';
-import Section from './Section.js';
 
 export default function Page() {
   return (
     <Section>
-      <Heading level={1}>Title</Heading>
-      <Heading level={2}>Heading</Heading>
-      <Heading level={3}>Sub-heading</Heading>
-      <Heading level={4}>Sub-sub-heading</Heading>
-      <Heading level={5}>Sub-sub-sub-heading</Heading>
-      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+      <Heading>Title</Heading>
+      <Heading>Heading</Heading>
+      <Heading>Sub-heading</Heading>
+      <Heading>Sub-sub-heading</Heading>
+      <Heading>Sub-sub-sub-heading</Heading>
+      <Heading>Sub-sub-sub-sub-heading</Heading>
     </Section>
   );
 }
