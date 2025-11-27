31738c4da515cca4ff32289616151ed29be6009e
Shamiul40 <shamiul4040@gmail.com>
2025-11-27 21:03:56 +0600

    context

--- module-2/src/App.jsx
+++ module-2/src/App.jsx
@@ -1,13 +1,3 @@
-import { useState } from 'react';
-import Chat from './Components/Chat/Chat';
-import ContactList from './Components/Chat/ContactList';
-
-const contacts = [
-  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
-  { id: 1, name: 'Alice', email: 'alice@mail.com' },
-  { id: 2, name: 'Bob', email: 'bob@mail.com' }
-];
-
 
 
 
