84b14a375c474e01d0cb3a151ef926c1f5f2531a
Shamiul40 <shamiul4040@gmail.com>
2025-12-10 00:35:30 +0600

    a

--- module-2/src/Components/Counter/Counter.jsx
+++ module-2/src/Components/Counter/Counter.jsx
@@ -18,21 +18,3 @@ export default function Counter() {
 
 
 
-// import { useRef, useState } from 'react';
-
-// export default function Counter() {
-// console.log("renderd")
-// const [click, setClick] = useState(0)
-
-//     const handleClick =()=>{
-//         setClick(click+1)
-//         console.log(click)
-//     }
-
-
-//   return (
-//     <button onClick={handleClick}>
-//       Click me!
-//     </button>
-//   );
-// }
