a54a9f4e17ee5017e4564c6a65bf86a09db6f2b2
Shamiul40 <shamiul4040@gmail.com>
2025-12-09 20:14:26 +0600

    a

--- module-2/src/Components/NestedState.jsx
+++ module-2/src/Components/NestedState.jsx
@@ -1,17 +1,2 @@
 import React from 'react'
-import { initialTravelPlan } from './data';
-import PlaceTree from './PlaceTree';
 
-export default function NestedState() {
-
-    const [plan, setPlan] = React.useState(initialTravelPlan);
-    const planets = plan.childPlaces;
-  return (
-    <div>
-      <h1>Places to visit</h1>
-      <ol>
-        {planets.map(place => <PlaceTree place={place} key={place.id} />)}
-      </ol>
-    </div>
-  )
-}
