418e023b7c7c8e7537db95bcdd2c246abe36fdf1
Shamiul40 <shamiul4040@gmail.com>
2025-12-10 21:51:34 +0600

    useRef

--- module-2/src/App.jsx
+++ module-2/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Counter from './Components/Counter/Counter'
+import UseRefLearn from './Components/UseRef/UseRefLearn'
+
 
 export default function App() {
   return (
     <div>
-      <Counter />
+      {/* <Counter /> */}
+    <UseRefLearn />
     </div>
   )
 }
---
+++ module-2/src/Components/UseRef/UseRefLearn.jsx
@@ -0,0 +1,18 @@
+import { useRef } from 'react';
+
+export default function UseRefLearn() {
+ const inputRef = useRef(null)
+
+ const handleClick=()=>{
+  inputRef.current.focus();
+ }
+  
+  return (
+    <>
+      <input ref={inputRef} />
+      <button onClick={handleClick} >
+        Focus the input
+      </button>
+    </>
+  );
+}
