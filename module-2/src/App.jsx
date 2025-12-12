9ab62007d0c095df13b012e06bb783f4d2a02a7a
Shamiul40 <shamiul4040@gmail.com>
2025-12-12 20:38:45 +0600

    git push

--- module-2/src/App.jsx
+++ module-2/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Counter from './Components/Counter/Counter'
 import UseRefLearn from './Components/UseRef/UseRefLearn'
+import UseEffectLearn from './Components/UseEffect/UseEffectLearn'
 
 
 export default function App() {
   return (
     <div>
       {/* <Counter /> */}
-    <UseRefLearn />
+    {/* <UseRefLearn /> */}
+    <UseEffectLearn />
     </div>
   )
 }
