94e17a8f7c518c60a34988d4fb1effea898968cf
Shamiul40 <shamiul4040@gmail.com>
2025-12-19 22:38:38 +0600

    hooks

--- module-2/src/Components/Form/Form.jsx
+++ module-2/src/Components/Form/Form.jsx
@@ -11,10 +11,10 @@ export default function Form() {
     
   return (
     <div>
-      <input type="text" value={firstNameProps} onChange={firstNameProps.handleChange}/>
+      <input type="text" {...firstNameProps}/>
       <br />
       <br />
-      <input type="text" value={lastNameProps} onChange={lastNameProps.handleChange} />
+      <input type="text" {...lastNameProps} />
       <div>
         <h1>Good Morning : {firstNameProps.value} {lastNameProps.value}</h1>
       </div>
