1710e92eb96b98584de75a6c954a57d7cd012efa
Shamiul40 <shamiul4040@gmail.com>
2025-12-19 22:08:38 +0600

    custom hooks learn

--- module-2/src/Components/Form/Form.jsx
+++ module-2/src/Components/Form/Form.jsx
@@ -1,25 +1,22 @@
 import React, { useState } from 'react'
+import useInput from '../../Hooks/useInput'
 
 export default function Form() {
-    const [firstName, setFirstName] = useState("shamiul")
-    const [LastName, setLastName] = useState("Islam")
+    
+    const firstNameProps = useInput("shamiul")
+    const lastNameProps = useInput("islam")
 
-    const handleFirstName =(e)=>{
-        setFirstName(e.target.value)
-    }
+   
 
-    const handleLastName =(e)=>{
-        setLastName(e.target.value)
-    }
-
+    
   return (
     <div>
-      <input type="text" value={firstName} onChange={handleFirstName}/>
+      <input type="text" value={firstNameProps} onChange={firstNameProps.handleChange}/>
       <br />
       <br />
-      <input type="text" value={LastName} onChange={handleLastName} />
+      <input type="text" value={lastNameProps} onChange={lastNameProps.handleChange} />
       <div>
-        <h1>Good Morning : {firstName} {LastName}</h1>
+        <h1>Good Morning : {firstNameProps.value} {lastNameProps.value}</h1>
       </div>
     </div>
   )
---
+++ module-2/src/Hooks/useInput.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+export default function useInput(initialValue) {
+ const  [value, setValue] = useState(initialValue)
+
+ const handleChange =(e)=>{
+    setValue(e.target.value)
+ }
+
+ return {
+    value,
+    onChange : handleChange
+ }
+
+}
