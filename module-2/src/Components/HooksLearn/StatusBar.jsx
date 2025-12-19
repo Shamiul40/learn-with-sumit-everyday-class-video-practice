70a56979f4c4aede6694d1c4a85401499d46241d
Shamiul40 <shamiul4040@gmail.com>
2025-12-19 18:58:20 +0600

    make custom hooks

--- module-2/src/Components/HooksLearn/StatusBar.jsx
+++ module-2/src/Components/HooksLearn/StatusBar.jsx
@@ -1,24 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import UseOnlineBar from '../../Hooks/UseOnlineBar'
 
 export default function StatusBar() {
-    const [isOnline, setIsOnline] = useState(navigator.onLine)
-
-    const handleOnline = ()=>{
-        setIsOnline(true)
-    }
-    const handleOffline =()=>{
-        setIsOnline(false)
-    }
-
-    useEffect(()=>{
-        window.addEventListener("online", handleOnline)
-        window.addEventListener("offline", handleOffline)
-
-        return ()=>{
-            window.removeEventListener("online", handleOnline)
-            window.removeEventListener("offline", handleOffline)
-        }
-    },[])
+    const isOnline = UseOnlineBar()
 
 
   return (
---
+++ module-2/src/Hooks/UseOnlineBar.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+export default function UseOnlineBar() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  const handleOnline = () => {
+    setIsOnline(true);
+  };
+  const handleOffline = () => {
+    setIsOnline(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+  return isOnline;
+}
