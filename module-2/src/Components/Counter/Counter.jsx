71ab7d3e7a98eba8c522b4c55144b42a08ec6f93
Shamiul40 <shamiul4040@gmail.com>
2025-12-27 22:05:58 +0600

    ad

--- module-2/src/Components/Counter/Counter.jsx
+++ module-2/src/Components/Counter/Counter.jsx
@@ -4,10 +4,6 @@ export default function Counter() {
     console.log("useRef rendered")
   let ref = useRef(0);
 
-  function handleClick() {
-    ref.current = ref.current + 1;
-    console.log('You clicked ' + ref.current + ' times!');
-  }
 
   return (
     <button onClick={handleClick}>
